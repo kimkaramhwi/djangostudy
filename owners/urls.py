e0a7415b46400b408242c109d2a78dfa4be0544d
kimkaramhwi <kimkrh@naver.com>
2022-02-15 15:50:32 +0900

    Actors and Movies

--- owners/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from owners.views import OwnersView, DogsView, OwnersDogsView
-
-urlpatterns = [
-    path('owners', OwnersView.as_view()),
-    path('dogs', DogsView.as_view()),
-    path('ownersdogs', OwnersDogsView.as_view())
-]
